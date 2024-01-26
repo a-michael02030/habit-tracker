acb2513af1d38f39c6ff200ae2ad8ef06f02f3a3
a-michael02030 <a_michael9999@yahoo.com>
2024-01-25 22:13:06 -0500

    Add Gatsby Plugin Netlify

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `HabitTracker`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [`gatsby-plugin-netlify`],
+}
